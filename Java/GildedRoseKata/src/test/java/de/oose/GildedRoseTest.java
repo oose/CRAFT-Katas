b7f69640dc796a903b49981ddca982657efb5bb3
MarkusL <markus.lang@oose.de>
2023-06-23 15:39:23 +0200

    fixed typos

--- Java/GildedRoseKata/src/test/java/de/oose/GildedRoseTest.java
+++ Java/GildedRoseKata/src/test/java/de/oose/GildedRoseTest.java
@@ -18,132 +18,132 @@ class GildedRoseTest {
 
 	@Test
 	public void standard_items_lower_sellIn_and_quality() {
-		createItemWithSellinAndquality(DEXTERITY_VEST, 10, 10);
-		doUpdatequality();
+		createItemWithSellinAndQuality(DEXTERITY_VEST, 10, 10);
+		doUpdateQuality();
 		verifyItemIs(DEXTERITY_VEST, 9, 9);
 	}
 
 	@Test
 	public void the_quality_of_an_item_is_never_negative() throws Exception {
-		createItemWithSellinAndquality(DEXTERITY_VEST, 10, 0);
-		doUpdatequality();
+		createItemWithSellinAndQuality(DEXTERITY_VEST, 10, 0);
+		doUpdateQuality();
 		verifyItemIs(DEXTERITY_VEST, 9, 0);
 	}
 
 	@Test
 	public void once_the_sell_by_date_has_passed_quality_degrades_twice_as_fast() throws Exception {
-		createItemWithSellinAndquality(DEXTERITY_VEST, 0, 10);
-		doUpdatequality();
+		createItemWithSellinAndQuality(DEXTERITY_VEST, 0, 10);
+		doUpdateQuality();
 		verifyItemIs(DEXTERITY_VEST, -1, 8);
 	}
 
 	@Test
 	public void once_the_sell_by_date_has_passed_quality_degrades_twice_as_fast_but_never_becomes_negative()
 			throws Exception {
-		createItemWithSellinAndquality(DEXTERITY_VEST, 0, 1);
-		doUpdatequality();
+		createItemWithSellinAndQuality(DEXTERITY_VEST, 0, 1);
+		doUpdateQuality();
 		verifyItemIs(DEXTERITY_VEST, -1, 0);
 	}
 
 	@Test
 	public void aged_brie_actually_increases_in_quality_the_older_it_gets() throws Exception {
-		createItemWithSellinAndquality(AGED_BRIE, 10, 10);
-		doUpdatequality();
+		createItemWithSellinAndQuality(AGED_BRIE, 10, 10);
+		doUpdateQuality();
 		verifyItemIs(AGED_BRIE, 9, 11);
 	}
 
 	@Test
 	public void the_quality_of_an_item_is_never_more_than_50() throws Exception {
-		createItemWithSellinAndquality(AGED_BRIE, 10, 50);
-		doUpdatequality();
+		createItemWithSellinAndQuality(AGED_BRIE, 10, 50);
+		doUpdateQuality();
 		verifyItemIs(AGED_BRIE, 9, 50);
 	}
 
 	@Test
 	public void sulfuras_being_a_legendary_item_never_has_to_be_sold_or_decreases_in_quality() throws Exception {
-		createItemWithSellinAndquality(SULFURAS_HAND_OF_RAGNAROS, 10, 80);
-		doUpdatequality();
+		createItemWithSellinAndQuality(SULFURAS_HAND_OF_RAGNAROS, 10, 80);
+		doUpdateQuality();
 		verifyItemIs(SULFURAS_HAND_OF_RAGNAROS, 10, 80);
 	}
 
 	@Test
 	public void backstage_passes_increases_in_quality_as_its_sellIn_value_approaches() throws Exception {
-		createBackstagePassWithSellinAndquality(20, 10);
-		doUpdatequality();
+		createBackstagePassWithSellinAndQuality(20, 10);
+		doUpdateQuality();
 		verifyBackstagePassIs(19, 11);
 
-		createBackstagePassWithSellinAndquality(11, 10);
-		doUpdatequality();
+		createBackstagePassWithSellinAndQuality(11, 10);
+		doUpdateQuality();
 		verifyBackstagePassIs(10, 11);
 	}
 
 	@Test
 	public void backstage_passes_with_sellIn_value_less_than_or_equal_to_10_increases_in_quality_by_two() {
-		createBackstagePassWithSellinAndquality(10, 10);
-		doUpdatequality();
+		createBackstagePassWithSellinAndQuality(10, 10);
+		doUpdateQuality();
 		verifyBackstagePassIs(9, 12);
 
-		createBackstagePassWithSellinAndquality(9, 10);
-		doUpdatequality();
+		createBackstagePassWithSellinAndQuality(9, 10);
+		doUpdateQuality();
 		verifyBackstagePassIs(8, 12);
 
-		createBackstagePassWithSellinAndquality(6, 10);
-		doUpdatequality();
+		createBackstagePassWithSellinAndQuality(6, 10);
+		doUpdateQuality();
 		verifyBackstagePassIs(5, 12);
 	}
 
 	@Test
 	public void backstage_passes_with_sellIn_value_less_than_or_equal_to_5_increases_in_quality_by_three() {
-		createBackstagePassWithSellinAndquality(5, 10);
-		doUpdatequality();
+		createBackstagePassWithSellinAndQuality(5, 10);
+		doUpdateQuality();
 		verifyBackstagePassIs(4, 13);
 
-		createBackstagePassWithSellinAndquality(4, 10);
-		doUpdatequality();
+		createBackstagePassWithSellinAndQuality(4, 10);
+		doUpdateQuality();
 		verifyBackstagePassIs(3, 13);
 
-		createBackstagePassWithSellinAndquality(1, 10);
-		doUpdatequality();
+		createBackstagePassWithSellinAndQuality(1, 10);
+		doUpdateQuality();
 		verifyBackstagePassIs(0, 13);
 	}
 
 	@Test
 	public void backstage_passes_with_sellIn_value_less_than_or_equal_to_0_drop_quality_to_0() {
-		createBackstagePassWithSellinAndquality(0, 10);
-		doUpdatequality();
+		createBackstagePassWithSellinAndQuality(0, 10);
+		doUpdateQuality();
 		verifyBackstagePassIs(-1, 0);
 
-		createBackstagePassWithSellinAndquality(-1, 10);
-		doUpdatequality();
+		createBackstagePassWithSellinAndQuality(-1, 10);
+		doUpdateQuality();
 		verifyBackstagePassIs(-2, 0);
 	}
 
 	@Test
 	public void backstage_passes_quality_never_increases_beyond_50() {
-		createBackstagePassWithSellinAndquality(20, 50);
-		doUpdatequality();
+		createBackstagePassWithSellinAndQuality(20, 50);
+		doUpdateQuality();
 		verifyBackstagePassIs(19, 50);
 
-		createBackstagePassWithSellinAndquality(9, 50);
-		doUpdatequality();
+		createBackstagePassWithSellinAndQuality(9, 50);
+		doUpdateQuality();
 		verifyBackstagePassIs(8, 50);
 
-		createBackstagePassWithSellinAndquality(3, 50);
-		doUpdatequality();
+		createBackstagePassWithSellinAndQuality(3, 50);
+		doUpdateQuality();
 		verifyBackstagePassIs(2, 50);
 	}
 
-	private void doUpdatequality() {
+	private void doUpdateQuality() {
 		gildedRose.updateQuality();
 	}
 
-	private void createItemWithSellinAndquality(String name, int sellIn, int quality) {
+	private void createItemWithSellinAndQuality(String name, int sellIn, int quality) {
 		Item[] items = new Item[] { new Item(name, sellIn, quality) };
 		gildedRose = new GildedRose(items);
 	}
 
-	private void createBackstagePassWithSellinAndquality(int sellIn, int quality) {
-		createItemWithSellinAndquality(BACKSTAGE_PASSES, sellIn, quality);
+	private void createBackstagePassWithSellinAndQuality(int sellIn, int quality) {
+		createItemWithSellinAndQuality(BACKSTAGE_PASSES, sellIn, quality);
 	}
 
 	private void verifyBackstagePassIs(int sellIn, int quality) {
