c990c1ff82aed4ef0b855f1909c303cbbc33d22a
Stephan Roth <StephanR@oose.de>
2025-01-20 18:00:53 +0100

    Added C# Katas.

---
+++ C#/TripServiceLegacyKata/Trip/TripDAO.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using TripServiceLegacyKata.Exception;
+
+namespace TripServiceLegacyKata.Trip
+{
+    public class TripDAO
+    {
+        public static List<Trip> FindTripsByUser(User.User user)
+        {
+            throw new DependendClassCallDuringUnitTestException
+                ("TripDAO should not be called when running a unit test!");
+        }
+    }
+}
---
+++ C#/TripServiceLegacyKata/User/UserSession.cs
@@ -0,0 +1,28 @@
+﻿using TripServiceLegacyKata.Exception;
+
+namespace TripServiceLegacyKata.User
+{
+    public class UserSession
+    {
+        private static readonly UserSession userSession = new UserSession();
+
+        private UserSession() { }
+
+        public static UserSession GetInstance()
+        {
+            return userSession;
+        }
+
+        public bool IsUserLoggedIn(User user)
+        {
+            throw new DependendClassCallDuringUnitTestException
+                ("UserSession.IsUserLoggedIn() should not be called when running a unit test!");
+        }
+
+        public User GetLoggedUser()
+        {
+            throw new DependendClassCallDuringUnitTestException
+                ("UserSession.GetLoggedUser() should not be called when running a unit test!");
+        }
+    }
+}
